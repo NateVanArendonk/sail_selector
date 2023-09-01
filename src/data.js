bb89b08e8ce40ab0909304a69a42f16ebc56e412
Nate VanArendonk <nathan.vanarendonk@gmail.com>
2023-08-31 19:55:57 -0700

    Update algo

--- src/data.js
+++ src/data.js
@@ -12,11 +12,14 @@ const wingSizeIncrement = 0.5;
 
 const constant1 = 17; //mph
 const constant2 = 5; //m
+const userWeightKg = 108; //kg
 
 for (let windSpeed = windSpeedStart; windSpeed <= windSpeedEnd; windSpeed += windSpeedIncrement) {
   for (let wingSize = wingSizeStart; wingSize <= wingSizeEnd; wingSize += wingSizeIncrement) {
-    const value = (Math.pow(windSpeed, 2) / (Math.pow(constant1, 2) * wingSize)) / constant2;
-    
+    const value = (windSpeed ** 2 / (constant1 ** 2) * wingSize / constant2 * (81 / userWeightKg))
+    // ideal_range[speed].append(speed**2 / 17**2 * size / 5 * (81 / user_weight_kg)) FROM PYTHON CODE
+    // ideal range = 0.9 - 1.5
+    // manageable = 0.8 - 0.9 & 1.5 - 2
     data.push({
       windSpeed,
       wingSize,
