7eae8485f036142886eb1d7cd5993ed5a40de3fc
Nate VanArendonk <nathan.vanarendonk@gmail.com>
2023-09-12 13:22:40 -0400

    Minor formatting edit

--- src/components/GraphHeader.js
+++ src/components/GraphHeader.js
@@ -4,9 +4,9 @@ import InformationModal from "./InformationModal";
 export default function GraphHeader() {
   return (
     <div className="flex items-center">
-      <h5 className="mb-2 mt-0 text-xl font-medium leading-tight text-secondary">
-        Find your ideal conditions to go winging
-      </h5>
+        <h3 className="mb-2 mt-0 text-3xl font-medium leading-tight text-primary">
+            Find your ideal conditions to go winging
+        </h3>
       <InformationModal />
     </div>
   );
