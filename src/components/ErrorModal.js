a6eab69d15482e33d16498c3fd03efcab02f1ae2
Nate VanArendonk <nathan.vanarendonk@gmail.com>
2023-09-12 12:01:32 -0400

    Update error modal

--- src/components/ErrorModal.js
+++ src/components/ErrorModal.js
@@ -18,10 +18,10 @@ export default function ErrorModal(props) {
         setShow={props.setShowErrorModalModal}
       >
         <TEModalDialog centered>
-          <TEModalContent>
+          <TEModalContent >
 
             <TEModalHeader>
-              <h5 className="text-xl font-medium leading-normal text-neutral-800 dark:text-neutral-200">
+              <h5 className="text-xl font-bold leading-normal text-neutral-800">
                 User Weight Error
               </h5>
               <button
@@ -30,20 +30,6 @@ export default function ErrorModal(props) {
                 onClick={() => props.setShowErrorModalModal(false)}
                 aria-label="Close"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="h-6 w-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
               </button>
             </TEModalHeader>
 
